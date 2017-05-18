fc8558a57441b682b693fb3a83bdde82fca10495
TomazMartins <tomaz.r.martins@gmail.com>
2017-05-18 00:39:54 -0300

    Creating component, serrvice and form for question
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- questions/src/app/app.module.ts
+++ questions/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { QuestionFormComponent } from './questions/question-form/question-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
